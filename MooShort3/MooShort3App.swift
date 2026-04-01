59e3acd222f5042ca0ffc0cccfe8997711f623e9
samtari1 <samtariyang@gmail.com>
2026-04-01 01:53:30 -0400

    Initial Commit

---
+++ MooShort3/MooShort3App.swift
@@ -0,0 +1,17 @@
+//
+//  MooShort3App.swift
+//  MooShort3
+//
+//  Created by Quanpeng Yang on 4/1/26.
+//
+
+import SwiftUI
+
+@main
+struct MooShort3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MooShort3Tests/MooShort3Tests.swift
@@ -0,0 +1,19 @@
+//
+//  MooShort3Tests.swift
+//  MooShort3Tests
+//
+//  Created by Quanpeng Yang on 4/1/26.
+//
+
+import Testing
+@testable import MooShort3
+
+struct MooShort3Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
